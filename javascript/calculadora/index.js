711a1dcc4b3d8a0afdeb98e6ec06c913a6ebc858
Kelvyn Reis <kelvyn2reis@gmail.com>
2022-02-24 15:45:55 -0300

    creat calculator with javascript basic

---
+++ javascript/calculadora/index.js
@@ -0,0 +1,75 @@
+function criarCalculadora (){
+
+    return {
+        display: document.querySelector('.display'),
+        
+        inicia(){
+            this.cliqueBotoes();
+            this.pressionaEnter();
+        },
+
+        pressionaEnter(){
+            this.display.addEventListener('keyup', e=>{
+                if(e.keyCode === 13){
+                    this.realizaConta();
+                }
+            })
+        },
+
+        clearDisplay(){
+            this.display.value = '';
+        },
+
+        apagaUm(){
+            this.display.value = this.display.value.slice(0, -1);
+        },
+
+        realizaConta(){
+            let conta = this.display.value;
+
+            try {
+                conta = eval(conta);
+
+                if(!conta) {
+                    alert('Conta invalida');
+                    return;
+                }
+
+                this.display.value = String(conta);
+            } catch(e){
+                alert('Conta invalida')
+                return;
+            }
+        },
+
+        cliqueBotoes(){
+            document.addEventListener('click', e =>{
+                const el = e.target;
+
+                if(el.classList.contains('btn-num')){
+                    this.btnParaDisplay(el.innerText);
+                }
+
+                if(el.classList.contains('btn-clear')){
+                    this.clearDisplay( )
+                }
+
+                if(el.classList.contains('btn-del')){
+                    this.apagaUm( )
+                }
+
+                if(el.classList.contains('btn-eq')){
+                    this.realizaConta( )
+                }
+            });
+        },
+
+        btnParaDisplay(valor){
+            this.display.value += valor
+        }
+
+    };
+}
+
+const calculadora = criarCalculadora();
+calculadora.inicia()
