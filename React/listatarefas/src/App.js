81a5830138bbd8312f0db48ede893e0102af4b19
Kelvyn Reis <kelvyn2reis@gmail.com>
2022-05-19 13:43:36 -0300

    configurete eslint

--- React/listatarefas/src/App.js
+++ React/listatarefas/src/App.js
@@ -1,7 +1,7 @@
+import React from "react";
 
-import './App.css'
-function App() {
-  return <h1>Hello world</h1>
+import "./App.css";
+
+export default function App() {
+  return <h1>Hello world</h1>;
 }
-
-export default App;
