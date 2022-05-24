9fd8057d9fcab4680e57839f23b34c353f3c22d2
Kelvyn Reis <kelvyn2reis@gmail.com>
2022-05-24 13:16:54 -0300

    add styled-components

---
+++ React/react-base/src/pages/Login/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Container } from '../../styles/GlobalStyles';
+
+export default function Login() {
+  return (
+    <Container>
+      <h1>Ola login</h1>
+    </Container>
+  );
+}
